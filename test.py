49df8d97ec009dcf37a05db27f3e3e11b3f57aee
Zhuohao Yu <zhuohaoyu1228@gmail.com>
2023-05-02 14:27:56 +0800

    Add pipeline sanity check

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from pandalm import EvaluationPipeline
 
 pipeline = EvaluationPipeline(
     candidate_paths=["./llama-7b-tuned/", "./bloom-7b-tuned/", "./opt-7b-tuned"],
-    input_data_path="/home/yzh/llm/PandaLM/data/sanity-check.json",
+    input_data_path="./data/pipeline-sanity-check.json",
 )
 print(pipeline.evaluate())
